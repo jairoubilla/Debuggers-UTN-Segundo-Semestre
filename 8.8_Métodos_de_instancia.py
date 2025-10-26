41b14d699d689e36aa4b8a3d52d9fe52acb5128e
jairoubilla <jwu.monk@gmail.com>
2025-10-26 08:59:50 -0300

    8.8 Métodos de instancia: Definimos un metodo
     8.8 Métodos de instancia: Definimos un metodo

---
+++ 8.8_Métodos_de_instancia.py
@@ -0,0 +1,6 @@
+# 8.8 Métodos de instancia: Definimos un metodo
+from Persona import persona1, persona2, persona3
+
+persona1.mostrar_detalles() # La referencia en este caso se pasa de manera automatica
+persona2.mostrar_detalles()
+persona3.mostrar_detalles()
