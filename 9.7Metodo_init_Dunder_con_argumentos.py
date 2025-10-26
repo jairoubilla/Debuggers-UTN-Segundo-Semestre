a5b2b5a6ba1a0614b1b438e9fec816834dcef80d
jairoubilla <jwu.monk@gmail.com>
2025-10-26 08:51:28 -0300

    9.7 Metodo init Dunder con argumentos variables
     9.7 Metodo init Dunder con argumentos variables

---
+++ 9.7Metodo_init_Dunder_con_argumentos.py
@@ -0,0 +1,5 @@
+# 9.7 Metodo init Dunder con argumentos variables
+from Persona import Persona
+
+persona4 = Persona('Lisandro', 'Martinez', 27, 'Teléfono', '2604234258', 'Calle Garcia', 980, 'Manzana', 87, 'Casa', 12, Altura=175, Peso=87, CFavorito='Negro', Auto='Ford', Modelo=98)
+persona4.mostrar_detalles()
