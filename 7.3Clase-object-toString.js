812753f3dca0623f743f9050d672f850b631ee68
jairoubilla <jwu.monk@gmail.com>
2025-10-27 01:28:15 -0300

    7.3 Clase Object, toString, sobreescritura y Polimorfismo
    7.3 Clase Object, toString, sobreescritura y Polimorfismo

---
+++ 7.3Clase-object-toString.js
@@ -0,0 +1,7 @@
+//7.3 Clase Object, toString, sobreescritura y Polimorfismo
+toString(){
+    return this.nombreCompleto();
+}
+
+console.log(empleado1.toString());
+console.log(persona1.toString());
