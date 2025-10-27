31fc341dac3bf34c303c5cf04c0c8add5054fedb
jairoubilla <jwu.monk@gmail.com>
2025-10-27 01:15:23 -0300

    6.1 Sintaxis de Clases en JavaScript: Parte 1 y 2
    6.1 Sintaxis de Clases en JavaScript: Parte 1 y 2

---
+++ 6.1.2-SintaxisDeClasesEnJS.js
@@ -0,0 +1,13 @@
+// 6.1 Sintaxis de Clases en JavaScript: Parte 1 y 2
+class Persona{//Clase padre
+    constructor(nombre, apellido){
+        this._nombre = nombre;
+        this._apellido = apellido;
+    }
+
+} 
+
+let persona1 = new Persona('Martin', 'Perez');
+console.log(persona1);
+let persona2 = new Persona('Carlos', 'Lara');
+console.log(persona2);
