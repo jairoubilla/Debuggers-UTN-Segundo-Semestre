16db1e05d84a855c09948b3d5078ed7a22fcb2f0
jairoubilla <jwu.monk@gmail.com>
2025-10-27 00:55:51 -0300

    4.4 Cómo acceder a las propiedades de los Objetos
    4.4 Cómo acceder a las propiedades de los Objetos

---
+++ 4.4Comoaccederalaspropiedades.js
@@ -0,0 +1,10 @@
+// Objeto
+let persona = {
+    nombre: 'Carlos',
+    apellido: 'Gil',
+    email: 'cgil@gmail.com',
+    edad: 30
+}
+
+console.log(persona['apellido']);
+console.log('Usamos el ciclo for in');
