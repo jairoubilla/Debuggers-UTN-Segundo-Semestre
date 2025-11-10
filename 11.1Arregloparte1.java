ca7f6ed9d8a6b1b828eba94a819c9bdd30561071
jairoubilla <jwu.monk@gmail.com>
2025-11-10 01:19:09 -0300

    11.1 Arreglos parte 1: Creamos la carpeta Leccion9
    11.1 Arreglos parte 1: Creamos la carpeta Leccion9

---
+++ 11.1Arregloparte1.java
@@ -0,0 +1,8 @@
+//11.1 Arreglos Parte 1: Creamos la carpeta Leccion9
+package test;
+
+public class TestArreglos {
+
+    public static void main(String[] args) { //Lado der. instanciamos un objeto de tipo object
+        int edades[] = new int[3]; // El lado izq. declaramos la variable
+        System.out.println("edades = " + edades);
